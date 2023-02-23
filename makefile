635cba0ffc09098b4256aa79a8f2ccd6cf73c2da
williamatku <morris.william@ku.edu>
2023-02-23 09:37:12 -0600

    final lab

--- makefile
+++ makefile
@@ -1,6 +1,11 @@
 prog: main.o matrix.o
 	g++ -g -std=c++11 -Wall main.o matrix.o -o prog
 
+dev:
+	make prog
+	./prog
+	make clean
+	
 main.o: main.cpp matrix.h
 	g++ -g -std=c++11 -Wall -c main.cpp
 
--- matrix.cpp
+++ matrix.cpp
@@ -24,12 +24,12 @@ void matrix::createMatrix()
 }
 
 void matrix::printMatrix(){
-        for(int i = 0; i<rows; i++){
-                for(int j = 0; j<cols; j++){
-                        std::cout<<grid[i][j]<<"\t";
-                }
-                std::cout<<"\n";
-        }
+	for(int i = 0; i<rows; i++){
+		for(int j = 0; j<cols; j++){
+				std::cout<<grid[i][j]<<"\t";
+		}
+		std::cout<<"\n";
+	}
 }
 
 void matrix::addMatrix(){
@@ -38,41 +38,83 @@ void matrix::addMatrix(){
 			grid_temp[i][j] = grid[i][j]+grid[i][j];
 		}
 	}
-        for(int i = 0; i<rows; i++){
-                for(int j = 0; j<cols; j++){
+	for(int i = 0; i<rows; i++){
+		for(int j = 0; j<cols; j++){
 			std::cout<<grid_temp[i][j]<<"\t";
-                }
+		}
 		std::cout<<"\n";
-        }
+	}
 }
 
+// William Morris, 3057236
 double matrix::getMax(){
-	//TO DO
-	double max_element = 0;
+	double max_element = 0; // init double var
+	for (int i = 0; i < this->rows; i++) {
+	for (int j = 0; j < this->cols; j++) {
+		if (this->grid[i][j] > max_element) max_element = this->grid[i][j]; // assign any bigger element to max element
+	}}
 	return max_element;
 }
 
+// William Morris, 3057236
 bool matrix::findElement(int no){
-	//TO DO
-	std::cout<<"\nElement not found\n";
-	return false;
+	bool found = 0; // no has not been found...
+	for (int i = 0; i < this->rows; i++) {
+	for (int j = 0; j < this->cols; j++) {
+		if (this->grid[i][j] == no) { // if no exists within the matrix
+			found = 1; // no has been found
+			printf("Element found at %d, %d !\n",i,j); // alert the user
+		};
+	}}
+	return found;
 }
 
+// William Morris, 3057236
 void matrix::changeElement(int r, int c, int no){
-	//TO DO
-	return;
-}
-void matrix::multiplyMatrix(){
-	//TO DO
-	//Hint: May want to store the new result in grid_temp and print that matrix
-	return;
+	if (r >= this->rows || c >= this->cols) {
+		return; // exit if r, c are out of bounds
+	}
+	this->grid[r][c] = no; // reassign grid at r, c = no
 }
 
-void matrix::transposeMatrix(){
-	//TO DO
-	//Hint: May want to store the new results in grid_temp and print that matrix
-	return;
+// William Morris, 3057236
+void matrix::multiplyMatrix(){
+	for(int i = 0; i<rows; i++){
+	for(int j = 0; j<cols; j++){
+		// For every matrix cell
+
+		// Clean val of cell
+		grid_temp[i][j] = 0;
+
+		// And add 
+		// every row element of matrix 1 * every col element of matrix 2
+		// To the final result
+		for (int k = 0; k<rows && k<cols; k++) {
+			grid_temp[i][j] += grid[i][k]*grid[k][j];
+		}
+	}}
+
+
+	// Print
+	for(int i = 0; i<rows; i++){
+		for(int j = 0; j<cols; j++){
+			std::cout<<grid_temp[i][j]<<"\t";
+		}
+		std::cout<<"\n";
+	}
 }
+
+// William Morris, 3057236
+void matrix::transposeMatrix() {
+	for(int i = 0; i<rows; i++){
+		for(int j = 0; j<cols; j++){
+			// print matrix in Transpose order, j, i vs i, j
+			std::cout<<grid[j][i]<<"\t";
+		}
+		std::cout<<"\n";
+	}
+}
+
 matrix::~matrix(){
         for(int i = 0; i<rows; i++){
                 delete[] grid[i];
